a86848826a7f7d8b98d4faf7a4497431163ae2e5
Parker Moore <parkrmoore@gmail.com>
2013-11-17 18:16:04 -0500

    Initial jekyll execution sturff

--- lib/octopress/commands/build.rb
+++ lib/octopress/commands/build.rb
@@ -2,7 +2,12 @@ module Octopress
   module Commands
     class Build
       def self.run(args, opts)
-        Octopress.logger("Executing build command")
+        Octopress.logger.debug("Executing build command")
+        self.execute_jekyll(opts)
+      end
+
+      def self.execute_jekyll(opts)
+        Jekyll::Commands::Build.process(Jekyll.configuration(opts))
       end
     end
   end
