572f0c5bce445ce7483b10d551375007732b1fd0
Parker Moore <parkrmoore@gmail.com>
2013-11-17 18:15:49 -0500

    Require all Octopress commands

--- lib/octopress.rb
+++ lib/octopress.rb
@@ -1,5 +1,16 @@
+this_dir = File.dirname(__FILE__)
+$LOAD_PATH.unshift(this_dir) unless $LOAD_PATH.include?(this_dir)
+
+def require_all(rel_dir)
+  path = File.expand_path(File.join(rel_dir, "*.rb"), File.dirname(__FILE__))
+  Dir[path].each { |f| require f }
+end
+
+# Octopress
 require "octopress/version"
 require "octopress/theme"
+require_all "octopress/commands"
+
 
 module Octopress
   def self.logger
