6cd641761bbf1f2c4fb65fe11e84e92c0b2afe10
Brandon Mathis <brandon@imathis.com>
2014-02-19 00:42:49 -0600

    Now docs requires the --jekyll flag to render jekyll docs

--- lib/octopress/commands/docs.rb
+++ lib/octopress/commands/docs.rb
@@ -6,24 +6,35 @@ module Octopress
     def self.init_with_program(p)
       p.command(:docs) do |c|
         c.syntax 'octopress docs'
-        c.description "Launch local server with docs for Jekyll v#{Jekyll::VERSION}"
+        c.description "Soon: Launch local server with docs for Octopress v#{Octopress::VERSION}"
 
         c.option 'port', '-P', '--port [PORT]', 'Port to listen on'
         c.option 'host', '-H', '--host [HOST]', 'Host to bind to'
+        c.option 'jekyll', '--jekyll', "Launch local server with docs for Jekyll v#{Jekyll::VERSION}"
 
         c.action do |args, options|
+          
+          # Only show jekyll docs if the jekyll flag was used
+          #
+          if options['jekyll']
+            options.delete('jekyll')
 
-          # Find local Jekyll gem path
-          spec = Gem::Specification.find_by_name("jekyll")
-          gem_path = spec.gem_dir
+            # Find local Jekyll gem path
+            #
+            spec = Gem::Specification.find_by_name("jekyll")
+            gem_path = spec.gem_dir
 
-          options = CommandHelpers.normalize_options(options)
-          options = ::Jekyll.configuration(options.to_symbol_keys.merge!({
-            'source' => "#{gem_path}/site",
-            'destination' => "#{gem_path}/site/_site"
-          }))
-          ::Jekyll::Commands::Build.process(options)
-          ::Jekyll::Commands::Serve.process(options)
+            options = CommandHelpers.normalize_options(options)
+            options = ::Jekyll.configuration(options.to_symbol_keys.merge!({
+              'source' => "#{gem_path}/site",
+              'destination' => "#{gem_path}/site/_site"
+            }))
+            
+            ::Jekyll::Commands::Build.process(options)
+            ::Jekyll::Commands::Serve.process(options)
+          else
+            puts "Sorry, not yet. View Octopress docs on http://octopress.org or view Jekyll docs locally by running `octopress docs --jekyll`"
+          end
         end
       end
     end
