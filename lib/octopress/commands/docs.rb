9e2189469f59428f215ef415537e5f22ef2e4944
Brandon Mathis <brandon@imathis.com>
2014-03-01 22:27:01 -0600

    Now serving plugin documentation.
    When octopress docs is run from within a Jekyll site, it will require
    all plugins listed in the _config.yml gems array, ensuring that any
    Octopress Ink plugins in the gems array will be able to add their
    documentation assets to the octopress docs site.

--- lib/octopress/commands/docs.rb
+++ lib/octopress/commands/docs.rb
@@ -1,4 +1,5 @@
 require 'jekyll'
+require 'yaml'
 require File.expand_path('helpers', File.dirname(__FILE__))
 
 module Octopress
@@ -16,35 +17,58 @@ module Octopress
         c.option 'jekyll', '--jekyll', "Launch local server with docs for Jekyll v#{Jekyll::VERSION}"
 
         c.action do |args, options|
-          
-          # Only show jekyll docs if the jekyll flag was used
-          #
-          if options['jekyll']
-            options.delete('jekyll')
-
-            # Find local Jekyll gem path
-            #
-            spec = Gem::Specification.find_by_name("jekyll")
-            gem_path = spec.gem_dir
-
-            options['source'] = "#{gem_path}/site",
-            options['destination'] = "#{gem_path}/site/_site"
-          else
-            options['source'] = site_dir
-            options['destination'] = File.join(site_dir, '_site')
-            Octopress.config({'octopress-config'=>File.join(site_dir, '_octopress.yml')})
-          end
-
-          options["serving"] = true
-          options = CommandHelpers.normalize_options(options)
-          options = ::Jekyll.configuration(options.to_symbol_keys)
-          ::Jekyll::Commands::Build.process(options)
-          ::Jekyll::Commands::Serve.process(options)
+          serve_docs(options)
         end
       end
     end
+
+    def self.serve_docs(options)
+      if options['jekyll']
+        options = init_jekyll_docs(options)
+      else
+        options = init_octopress_docs(options)
+      end
+      options["serving"] = true
+      options = CommandHelpers.normalize_options(options)
+      options = ::Jekyll.configuration(options.to_symbol_keys)
+      ::Jekyll::Commands::Build.process(options)
+      ::Jekyll::Commands::Serve.process(options)
+    end
+
+    def self.init_octopress_docs(options)
+      Octopress.config({'octopress-config'=>File.join(site_dir, '_octopress.yml')})
+      require_gems
+      options['source'] = site_dir
+      options['destination'] = File.join(site_dir, '_site')
+      options
+    end
+
+    def self.init_jekyll_docs(options)
+      options.delete('jekyll')
+
+      # Find local Jekyll gem path
+      #
+      spec = Gem::Specification.find_by_name("jekyll")
+      gem_path = spec.gem_dir
+
+      options['source'] = "#{gem_path}/site",
+      options['destination'] = "#{gem_path}/site/_site"
+      options
+    end
+
     def self.site_dir
       File.expand_path('docs', File.join(File.dirname(__FILE__), '../../../', ))
+    end
+
+    def self.require_gems
+      file = File.join(Dir.pwd, '_config.yml')
+      if File.exist? file
+        config = YAML.safe_load(File.open(file))
+        gems = config['gems']
+        if gems && gems.is_a?(Array)
+          gems.each {|g| require g }
+        end
+      end
     end
   end
 end
