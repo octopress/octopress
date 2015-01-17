ba51819b562253c35d915a4125991d20ffe133a9
Brandon Mathis <brandon@imathis.com>
2015-01-17 09:55:18 -0600

    Updated docs to add --lang flag

--- lib/octopress/commands/helpers.rb
+++ lib/octopress/commands/helpers.rb
@@ -5,7 +5,7 @@ module Octopress
     def add_page_options(c)
       c.option 'date',     '-d', '--date DATE', "Use 'now' or a String that is parseable by Time#parse."
       c.option 'template', '-tm', '--template PATH', "New #{c.name.to_s} from a template."
-      c.option 'lang',     '-l', '--lang LANGUAGE', "Set a #{c.name.to_s} language (e.g. en, it, du) for multi-language sites."
+      c.option 'lang',     '-l', '--lang LANGUAGE', "Set a #{c.name.to_s} language (e.g. en, it) for multi-language sites."
       c.option 'force',    '-f', '--force', 'Overwrite file if it already exists'
     end
 
