666004eb256be1926300188186ce3e24a7ed7715
Brandon Mathis <brandon@imathis.com>
2015-07-27 00:04:22 -0500

    Attempt to force encoding of template files. #131

--- lib/octopress/page.rb
+++ lib/octopress/page.rb
@@ -133,7 +133,7 @@ module Octopress
         file.sub!(/^_templates\//, '')
         file = File.join(site.source, '_templates', file) if file
         if File.exist? file
-          parse_template File.open(file).read
+          parse_template File.open(file).read.encode('UTF-8')
         elsif @options['template']
           abort "No #{@options['type']} template found at #{file}"
         else
--- lib/octopress/version.rb
+++ lib/octopress/version.rb
@@ -1,3 +1,3 @@
 module Octopress
-  VERSION = "3.0.11"
+  VERSION = "3.0.12.pre.1"
 end
